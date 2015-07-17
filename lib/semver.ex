44e049c171464b0308ea47511cc1ec11eb73278a
Lee Dohm <lee@lee-dohm.com>
2015-07-16 23:27:50 -0700

    :memo: Add basic documentation

--- lib/semver.ex
+++ lib/semver.ex
@@ -1,4 +1,11 @@
 defmodule Semver do
+  @moduledoc """
+  Utilities for working with semver-compliant version strings.
+  """
+
+  @doc """
+  Validates a version string.
+  """
   def is_valid(version) do
     version =~ ~r/^v?(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?(\+[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?$/
   end
