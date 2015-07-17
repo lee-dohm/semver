55635d816a395804c03ffad78fa210c4b599506c
Lee Dohm <lee@lee-dohm.com>
2015-07-17 00:34:41 -0700

    Change is_valid to valid?

--- lib/semver.ex
+++ lib/semver.ex
@@ -6,7 +6,16 @@ defmodule Semver do
   @doc """
   Validates a version string.
   """
-  def is_valid(version) do
+  def valid?(version) do
     version =~ ~r/^v?(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?(\+[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?$/
+  end
+
+  def split(version) do
+    [major_text, minor_text, patch_text] = String.split(version, ~r/\./)
+    {major, _} = Integer.parse(major_text)
+    {minor, _} = Integer.parse(minor_text)
+    {patch, _} = Integer.parse(patch_text)
+
+    %{major: major, minor: minor, patch: patch}
   end
 end
