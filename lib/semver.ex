c946c883593db3a3a8a5ea5ad765f7ef80a6252b
Lee Dohm <lee@lee-dohm.com>
2015-07-17 08:49:55 -0700

    Revert "Change is_valid to valid?"
    This reverts commit 55635d816a395804c03ffad78fa210c4b599506c.

--- lib/semver.ex
+++ lib/semver.ex
@@ -6,16 +6,7 @@ defmodule Semver do
   @doc """
   Validates a version string.
   """
-  def valid?(version) do
+  def is_valid(version) do
     version =~ ~r/^v?(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?(\+[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?$/
-  end
-
-  def split(version) do
-    [major_text, minor_text, patch_text] = String.split(version, ~r/\./)
-    {major, _} = Integer.parse(major_text)
-    {minor, _} = Integer.parse(minor_text)
-    {patch, _} = Integer.parse(patch_text)
-
-    %{major: major, minor: minor, patch: patch}
   end
 end
--- spec/semver_spec.exs
+++ spec/semver_spec.exs
@@ -1,40 +1,34 @@
 defmodule SemverSpec do
   use ESpec
 
-  describe "valid?" do
-    it "accepts a standard version number", do: expect(Semver.valid?("1.1.1")).to eq(true)
-    it "accepts a version with leading v", do: expect(Semver.valid?("v1.1.1")).to eq(true)
+  describe "is_valid" do
+    it "accepts a standard version number", do: expect(Semver.is_valid("1.1.1")).to eq(true)
+    it "accepts a version with leading v", do: expect(Semver.is_valid("v1.1.1")).to eq(true)
 
     it "does not accept other leading characters" do
-      expect(Semver.valid?("z1.1.1")).to eq(false)
+      expect(Semver.is_valid("z1.1.1")).to eq(false)
     end
 
     it "does not accept random trailing characters" do
-      expect(Semver.valid?("1.1.1vvv")).to eq(false)
+      expect(Semver.is_valid("1.1.1vvv")).to eq(false)
     end
 
-    it "accepts multi-digit segments", do: expect(Semver.valid?("11.11.11")).to eq(true)
-    it "accepts all zeros", do: expect(Semver.valid?("0.0.0")).to eq(true)
-    it "does not accept leading zeros", do: expect(Semver.valid?("01.01.01")).to eq(false)
-    it "accepts pre-release versions", do: expect(Semver.valid?("1.1.1-alpha")).to eq(true)
+    it "accepts multi-digit segments", do: expect(Semver.is_valid("11.11.11")).to eq(true)
+    it "accepts all zeros", do: expect(Semver.is_valid("0.0.0")).to eq(true)
+    it "does not accept leading zeros", do: expect(Semver.is_valid("01.01.01")).to eq(false)
+    it "accepts pre-release versions", do: expect(Semver.is_valid("1.1.1-alpha")).to eq(true)
 
     it "accepts dotted pre-release versions" do
-      expect(Semver.valid?("1.1.1-alpha.beta")).to eq(true)
+      expect(Semver.is_valid("1.1.1-alpha.beta")).to eq(true)
     end
 
-    it "accepts build versions", do: expect(Semver.valid?("1.1.1+alpha")).to eq(true)
+    it "accepts build versions", do: expect(Semver.is_valid("1.1.1+alpha")).to eq(true)
     it "accepts dotted build versions" do
-      expect(Semver.valid?("1.1.1+alpha.beta")).to eq(true)
+      expect(Semver.is_valid("1.1.1+alpha.beta")).to eq(true)
     end
 
     it "accepts both pre-release and build versions" do
-      expect(Semver.valid?("1.1.1-alpha.beta+12345.67890")).to eq(true)
-    end
-  end
-
-  describe "split" do
-    it "splits a normal version" do
-      expect(Semver.split("0.0.0")).to eq(%{major: 0, minor: 0, patch: 0})
+      expect(Semver.is_valid("1.1.1-alpha.beta+12345.67890")).to eq(true)
     end
   end
 end
