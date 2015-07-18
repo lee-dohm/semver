1390566880f4ee44046e0d87549fa4b673831f04
Lee Dohm <lee@lee-dohm.com>
2015-07-18 13:07:06 -0700

    Prepare for publishing to Hex

--- mix.exs
+++ mix.exs
@@ -1,42 +1,41 @@
 defmodule Semver.Mixfile do
   use Mix.Project
 
+  @homepage "https://github.com/lee-dohm/semver"
+  @version File.read!("VERSION") |> String.strip
+
   def project do
     [
       app: :semver,
-      name: "Semver",
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.0",
-      source_url: "https://github.com/lee-dohm/semver",
-      homepage_url: "https://github.com/lee-dohm/semver",
+      source_url: @homepage,
+      homepage_url: @homepage,
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       preferred_cli_env: [espec: :test],
-      deps: deps
+      deps: deps,
+      package: package
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: []]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [
       {:espec, "~> 0.6.3", only: :test},
       {:ex_doc, "~> 0.7", only: :dev},
       {:earmark, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp package do
+    [
+      files: ~w(lib mix.exs README.md LICENSE.md VERSION CHANGELOG.md),
+      licenses: ["MIT"],
+      links: %{"GitHub" => @homepage}
     ]
   end
 end
